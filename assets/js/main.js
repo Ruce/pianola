f9b1d247cab4b406f16d0958c3a870fc99119db0
Ruce <bruce.togo@gmail.com>
2023-10-02 13:01:05 +0100

    Deploy new model and tweak info overlay

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,6 +108,10 @@ function playExample(exampleNum) {
 	piano.playExample(song.data, song.bpm);
 }
 
+function openOverlay() {
+    document.getElementById('overlay').style.display = 'block';
+}
+
 function closeOverlay() {
     document.getElementById('overlay').style.display = 'none';
 }
--- sagemaker/code/inference.py
+++ sagemaker/code/inference.py
@@ -304,7 +304,7 @@ def model_fn(model_dir):
         vocab_size=0,
         hidden_size=128,
         intermediate_size=2048,
-        num_hidden_layers=6,
+        num_hidden_layers=8,
         num_attention_heads=8,
         num_key_value_heads=None,
         hidden_act="silu",
