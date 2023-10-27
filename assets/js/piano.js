1bd4055a5166baa97d7dbcd9e38f03a5d008c1fd
Ruce <bruce.togo@gmail.com>
2023-10-27 16:09:00 +0100

    Add piano roll canvas and format history list

--- assets/js/piano.js
+++ assets/js/piano.js
@@ -403,10 +403,19 @@ class Piano {
 		
 		const listContainer = document.getElementById('historyDrawerList');
 		const historyElement = document.createElement('li');
-		historyElement.textContent = history.name;
-		if (history.start !== null) historyElement.textContent += ` (${history.start.toTimeString().split(' ')[0]})`;
+		const pianoRoll = new PianoRoll();
+		const textElement = document.createElement('div');
+		historyElement.appendChild(pianoRoll.canvas);
+		historyElement.appendChild(textElement);
+		
+		textElement.classList.add('historyTextContainer');
+		textElement.innerHTML = `<span class="historyTitle">${history.name}</span>`;
+		const dateOptions = {day: '2-digit', month: 'short', hour: '2-digit', minute: '2-digit', second: '2-digit'};
+		if (history.start !== null) textElement.innerHTML += `<span class="historyDescription">${history.start.toLocaleString('en-US', dateOptions)}</span>`;
 		historyElement.addEventListener('click', () => this.replayHistory(historyIdx));
 		listContainer.appendChild(historyElement);
+		
+		pianoRoll.draw(history);
 	}
 	
 	replayHistory(idx) {
@@ -759,6 +768,46 @@ class PianoCanvas {
 		this.animationQueued = false;
 		if (this.piano.activeNotes.length > 0) {
 			this.triggerDraw();
+		}
+	}
+}
+
+class PianoRoll {
+	constructor() {
+		this.canvas = PianoRoll.createCanvasElement('164px', '92px', 'pianoRoll');
+	}
+	
+	// Left X coordinate of each black key relative to start of an octave
+	static get blackKeyX() {
+		return [2, 6, 14, 18, 22];
+	}
+	
+	static createCanvasElement(width, height, className) {
+		const canvasElement = document.createElement('canvas');
+		canvasElement.style.width = width;
+		canvasElement.style.height = height;
+		if (className) canvasElement.classList.add(className);
+		return canvasElement;
+	}
+	
+	draw(history) {
+		const ctx = this.canvas.getContext('2d');
+		this.canvas.width = this.canvas.offsetWidth;
+		this.canvas.height = this.canvas.offsetHeight;
+		
+		const padding = 6;
+		ctx.fillStyle = "#2A2A2A";
+		ctx.beginPath();
+		ctx.roundRect(0, 0, this.canvas.width, this.canvas.height, padding);
+		ctx.fill();
+		
+		for (const note of history.noteHistory) {
+			const x = note.key.isWhiteKey? padding + note.key.colourKeyNum * 4 : padding + ((note.key.octave * 28) - 20) + PianoRoll.blackKeyX[(note.key.colourKeyNum - 1) % 5];
+			const y = padding + note.time * 15;
+			const width = note.key.isWhiteKey? 3 : 2;
+			const height = note.duration * 12;
+			ctx.fillStyle = note.key.isWhiteKey ? NoteBar.fill[note.actor.name].white : NoteBar.fill[note.actor.name].black;
+			ctx.fillRect(x, y, width, height);
 		}
 	}
 }
