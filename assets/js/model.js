7229d2ade4f6e71d3aa1c02645e22eb0e9273f8f
Ruce <bruce.togo@gmail.com>
2024-01-24 15:47:41 +0000

    Add autocorrection functionality for player input

--- assets/js/model.js
+++ assets/js/model.js
@@ -68,13 +68,14 @@ class PianolaModel {
 		return generated;
 	}
 	
-	static historyToQueryString(noteHistory, start, end, interval) {
+	static historyToQueryString(noteHistory, start, end, interval, autoCorrect=false) {
 		/*
 		Arguments:
 			`noteHistory`: an array containing recent history of Notes that were played
 			`start`: the TransportTime (in seconds) for the start of history period
 			`end`: the TransportTime (in seconds) for the non-inclusive end of history period
 			`interval`: length (in seconds) of a tick interval
+			`autoCorrect`: if enabled, align notes to lower fidelity in order to reduce player mistakes
 		*/
 		
 		function ceilWithPrecision(num, precision) {
@@ -83,14 +84,22 @@ class PianolaModel {
 			return truncatedNum;
 		}
 		
-		const numSixteenthNotes = ceilWithPrecision((end - start) / interval, 8);
-		const orderedNotes = Array.from({ length: numSixteenthNotes }, () => []);
+		const numNotes = ceilWithPrecision((end - start) / interval, 8);
+		const orderedNotes = Array.from({ length: numNotes }, () => []);
 
 		for (const n of noteHistory) {
 			if (n.time < end) {
-				const t = Math.floor((n.time - start) / interval); // Delta between note and startTick in SixteenthNotes
+				let t, duration;
 				const velocity = Math.max(Math.min(Math.round((n.velocity * 100) - 1), 99), 0); // Velocity scaled to be 0-indexed, between 0 and 99
-				const duration = Math.max(Math.min(Math.round(n.duration / interval * 10), 999), 1);
+				
+				if (autoCorrect && n.actor === Actor.Player) {
+					const numIntervals = (n.time - start) / interval; // Number of intervals between startTick and note
+					t = Math.floor((numIntervals + 0.5) / 2) * 2; // Fit the note to the closest even-numbered interval
+					duration = Math.max(Math.min(Math.round(n.duration / interval * 2) * 5, 999), 1); // Scale duration to have precision up to half an interval
+				} else {
+					t = Math.floor((n.time - start) / interval);
+					duration = Math.max(Math.min(Math.round(n.duration / interval * 10), 999), 1); // Scale duration by 10 so that it has 1/10th precision
+				}
 				
 				// N.B. Important: we shift the note up by 2 so that the base52 string is guaranteed to be 4 characters long
 				const noteShifted = n.key.keyNum + 2
@@ -174,7 +183,7 @@ class PianolaModel {
 		const delayMs = 200;
 		
 		if (this.isConnected) {
-			const queryString = PianolaModel.historyToQueryString(history, start, end, interval);
+			const queryString = PianolaModel.historyToQueryString(history, start, end, interval, true);
 			console.log(new Date().toISOString(), 'Query:', queryString);
 			const data = await this.queryModel(queryString, timesteps, numRepeats, selectionIdx, maxRetries, delayMs);
 			console.log(new Date().toISOString(), 'Data:', data);
